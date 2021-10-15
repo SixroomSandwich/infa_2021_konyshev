526f533456a5bc0a9135e40085f0ea001c377856
SixroomSandwich <konyshev.md@phystech.edu>
2021-10-15 09:29:45 +0300

    Добавил комментарии для большинства функций, пренёс все переменные и константы в верхнюю часть программы

--- Laba 5/balls_clicker.py
+++ Laba 5/balls_clicker.py
@@ -6,13 +6,26 @@ import time
 
 pygame.init()
 
+'''Переменные, связанные с параметрами экрана и  скоростью программы'''
 FPS = 30
 display_height = 900
 display_width = 900
 panel_height = 200
 panel_width = 900
 
-screen = pygame.display.set_mode((display_height, display_width))
+'''Переменные, отвечающие за кол-во набранных очков и за кол-во ошибок'''
+score = 0
+misses = 0
+misses_flag = 0
+gotcha_flag = 0
+
+'''Кол-во одновременно существующих шариков'''
+balls_number = 2
+
+'''Переменные, отвечающие за время жизни шарика'''
+max_life_time = 100
+life_time = [0] * balls_number
+deth_counter = [0] * balls_number
 
 '''Добавляем цвета'''
 RED = (255, 0, 0)
@@ -38,7 +51,10 @@ LAVENDER = (230, 230, 250)
 COLORS = [DARK_VIOLET, BLUE_VIOLET, MEDIUM_PURPLE, MEDIUM_ORCHID, MAGENTA, ORCHID, VIOLET, PLUM, THISTLE, LAVENDER]
 
 def draw_display(misses):
-    '''Функция обновляет экран (для стирания шариков и для отображения верхней панели)'''
+    '''
+    Функция обновляет экран (стиранает шарики и для орисует верхнюю панель)
+    misses - кол-во ошибок (кол-во жизней = 3 - кол-во ошибок)
+    '''
     screen.fill(BLACK)
     rect(screen, WHITE, (0, 0, panel_width, panel_height))
 
@@ -47,15 +63,20 @@ def draw_display(misses):
     textsurface = myfont.render(str(score), False, BLACK)
     screen.blit(textsurface, (50, 25))
 
-    '''Здесь функция отображает оставшиеся "жизни" игрока'''
+    '''Здесь функция рисует оставшиеся "жизни" игрока'''
     for life_num in range(3 - misses):
             draw_heart(screen, RED, panel_width // 2 + panel_width // 6 * life_num, panel_height // 4, panel_width // 9, panel_height // 9)
 
 def draw_endgame_display(score):
-    '''Функция рисует дисплей после проигрыша'''
+    '''
+    Функция рисует экран после проигрыша
+    score - кол-во очков, набранное игроком за игру
+    '''
+    '''Рисует чёрный экран и белую панель сверху'''
     screen.fill(BLACK)
     rect(screen, WHITE, (0, 0, panel_width, panel_height))
 
+    '''Пишет прощальную фразу и счёт'''
     myfont = pygame.font.SysFont('Comic Sans MS', 45)
     
     text_letters = myfont.render("GAME OVER. YOU'R SCORE IS ", False, BLACK)
@@ -79,13 +100,21 @@ def draw_heart(surface, color, x, y, width, heigth):
 def draw_ball(x, y, r):
     '''
     Рисует шарик 
+    x, y - координаты центра шарика
+    r - радиус шарика
     Цвет шарика зависит от его радиуса
     '''
     color = COLORS[r // 10 - 1]
     circle(screen, color, (x, y), r)
 
 def new_coordinates():
-    '''Функция отдаёт рандомные координаты, скорости и размеры для шариков'''
+    '''
+    Функция отдаёт рандомные координаты, скорости и размеры для шариков
+    На выходе отдаёт массив из пяти эллементов:
+    0, 1 эллементы - координаты центра шарика x, y
+    2,3 эллементы - скорости вдоль каждой оси v_x, v_y
+    4 эллемент - радиус шарика r
+    '''
     coordinates = [0] * 5
     coordinates[0] = randint(109, display_width - 109)
     coordinates[1] = randint(panel_height + 109, display_height - 109)
@@ -109,28 +138,19 @@ def ball_moving(x, y, v_x, v_y, r):
 
     return [x, y, v_x, v_y, r]
 
+
+screen = pygame.display.set_mode((display_height, display_width))
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-
-
-'''Переменные, отвечающие за кол-во набранных очков и за кол-во ошибок'''
-score = 0
-misses = 0
-misses_flag = 0
-gotcha_flag = 0
-
-balls_number = 2
-'''Переменные, отвечающие за время жизни шарика'''
-max_life_time = 100
-life_time = [0] * balls_number
-deth_counter = [0] * balls_number
-
 draw_display(misses)
+
+'''Задаем параметры для всех шариков'''
 coordinates = [[0] * 5 for i in range(balls_number)]
 for num in range(balls_number):
     coordinates[num] = new_coordinates()
 
+'''Начинаем играть'''
 while not finished:
     clock.tick(FPS)
     points = 0
@@ -147,14 +167,23 @@ while not finished:
                     points = (119 - coordinates[num][4]) // 10
                     print("Gotcha! +", points, "points!")
                     score += points
+                    '''В специальном массиве отмечаем смерть шарика с номеров num'''
                     deth_counter[num] += 1
+                    '''
+                    Фиксируем попадание по одному из шариков (В дальнейшем это используется, чтобы понять, что игрок попал по шарику,
+                    так как для других шариков программа зафиксирует промах)
+                    '''
                     gotcha_flag = 1
                 else:
-                    '''Если игрок кликнул, но не попал по кружочку, то кол-во его ошибок увеличивается на 1'''
+                    '''
+                    Если игрок кликнул, но не попал по кружочку, то мы фиксируем, что в этом цикле была совершенна ошибка 
+                    (Делаем так, а не просто misses += 1, чтобы за одну ошибку программа не отнимала кол-во жизней, равное balls_number)
+                    '''
                     misses_flag = 1
 
+    '''Проверяем, была ли совершенна ошибка, если да, то отнимаем жизнь'''
     if gotcha_flag == 0 and misses_flag == 1:
-        misses += misses_flag
+        misses += 1
     misses_flag = 0
     gotcha_flag = 0
     draw_display(misses)
@@ -163,9 +192,10 @@ while not finished:
             '''Игрок попал по шарику, создаётся новый шарик'''
             coordinates[num] = new_coordinates()
             draw_ball(coordinates[num][0], coordinates[num][1], coordinates[num][4])
+            '''Так же продлевается время жизни оставшихся шариков'''
             for i in range(balls_number):
                 if i != num:
-                    life_time[i] -= 20
+                    life_time[i] -= max_life_time // 5
                 life_time[num] = 0
             deth_counter[num] = 0
 
@@ -183,8 +213,8 @@ while not finished:
             life_time[num] += 1
             pygame.display.flip()
             
-        '''Игра заканчивается, если у игрока не остаётся жизней'''
-    if misses == 3:
+    '''Игра заканчивается, если у игрока не остаётся жизней'''
+    if misses >= 3:
 
         draw_endgame_display(score)
         pygame.display.flip()
